a0b5296b840e150cd054881967a8ec8a42b5d9b6
binod <t17711@users.noreply.github.com>
2016-12-06 18:35:53 -0500

    added apple version for blur convolve and corr

---
+++ hw2/fshader_convolve_Mac.glsl
@@ -0,0 +1,33 @@
+#version 120
+
+varying	vec2	  v_TexCoord;	// varying variable for passing texture coordinate from vertex shader
+
+uniform int       u_Wsize;	// blur width value
+uniform int       u_Hsize;	// blur height value
+uniform float	  u_WStep;
+uniform float	  u_HStep;
+uniform	sampler2D u_Sampler;	// uniform variable for the texture image
+uniform	float[1000] u_Weight; // uniform variable for the texture image
+
+void main() {
+	vec3 avg = vec3(0.0);
+	vec2 tc  = v_TexCoord;
+	vec3 wt = vec3(0.0);
+	int  w2  = u_Wsize / 2;
+	int  h2  = u_Hsize / 2;
+
+	int sz = (u_Wsize*u_Hsize);
+	float wt_hstep = 1.0/u_Hsize;
+	float wt_wstep = 1.0/u_Wsize;
+
+	for(int i=-h2, ii =0; i<=h2; ++i){
+		for(int j=-w2; j<=w2; ++j,++ii){
+			if (ii==sz) ii = 0;// just for security
+			wt = vec3(u_Weight[ii],u_Weight[ii],u_Weight[ii]);
+			avg += (texture2D(u_Sampler, vec2(tc.x + i*u_WStep, tc.y + j*u_HStep)).rgb*wt);
+		}
+	}
+	//avg = avg / (u_Wsize * u_Hsize);
+	gl_FragColor = vec4(avg, 1.0);
+	
+}
---
+++ hw2/fshader_correlation_Mac.glsl
@@ -0,0 +1,90 @@
+#version 120
+
+varying	vec2	  v_TexCoord;	// varying variable for passing texture coordinate from vertex shader
+
+// for sample get its step and colors
+uniform float u_WStep_s;
+uniform float u_HStep_s;
+
+// for kernel
+uniform int       u_Wsize_k;	
+uniform int       u_Hsize_k;
+uniform float	  u_WStep_k;
+uniform float	  u_HStep_k;
+uniform	sampler2D u_Sampler;
+uniform	sampler2D u_Kernel; 
+uniform int u_Color;
+uniform int u_passthrough;
+
+void main() {
+
+	vec2 tc  = v_TexCoord;
+	vec3 sm1 =vec3(0.0f);
+	vec3 sm2 =vec3(0.0f);
+	vec3 sm3 =vec3(0.0f);
+	float sum1 = 1.0f;
+	float sum2 = 1.0f;
+	vec3 image=	vec3(0.0f);
+	vec3 kernel = vec3(0.0f);
+	vec3 pt1 = vec3(0.0);
+	vec3 pt2 = vec3(0.0);
+
+	vec3 tavg = vec3(0.0);
+	vec3 iavg = vec3(0.0);
+
+	if (u_passthrough == 0 ){
+		vec3 s= vec3(0.3f,0.6f,0.1f);
+		//vec3 s2= vec3(0.0);
+
+		int factor = 2; // because gpu hanged
+
+		for(int i = 0; i<= u_Hsize_k; i+=factor){
+			for (int j =0; j <= u_Wsize_k; j+=factor){
+				pt1 = (texture2D(u_Sampler,vec2(tc.x + i*u_WStep_s, tc.y + j *u_HStep_s)).rgb);
+				pt2 = (texture2D(u_Kernel,vec2(i*u_WStep_k, j *u_HStep_k)).rgb);
+				tavg+= pt1;
+				iavg+= pt2;
+				}
+			}
+
+			tavg/=(u_Hsize_k*u_Wsize_k);
+			iavg/=(u_Hsize_k*u_Wsize_k);
+
+			tavg*=factor;
+			iavg*=factor;
+
+		for(int i = 0; i<= u_Hsize_k; i+=factor){
+			for (int j =0; j <= u_Wsize_k; j+=factor){
+				
+			//	CORR_COEFF (correlation coefficient):
+			//		C(u,v) = sum of {(T(x,y)-Tavg) * (I(x-u,y-v)-Iavg)}
+			//			 -----------------------------------------------------
+			//			 sqrt{sum{(T(x,y)-Tavg)^2} * sum{(I(x-u,y-v)-Iavg)^2}}
+
+				pt1 = (texture2D(u_Sampler,vec2(tc.x + i*u_WStep_s, tc.y + j *u_HStep_s)).rgb);
+				pt2 = (texture2D(u_Kernel,vec2(i*u_WStep_k, j *u_HStep_k)).rgb);
+				vec3 a = (pt2-tavg);
+				vec3 b = (pt1-iavg);
+
+				sm1 += a*b;
+				sm2 += a*a;	
+				sm3 += b*b;	
+			}
+		}
+
+		sm2= inversesqrt(sm2*sm3);
+		
+		sm1 = sm1*sm2*s;
+
+		if(u_Color ==1){
+				sum1 = clamp(sm1.r+sm1.g+sm1.b,0.0f,1.0f);
+		}
+		else 
+			sum1 = clamp(sm1.r,0.0f,1.0f);
+		gl_FragColor = vec4(sum1,sum1,sum1,sum1);
+	}
+	else{
+			gl_FragColor = (texture2D(u_Sampler,v_TexCoord));
+	}
+
+}
